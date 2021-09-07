7ed84749e0a0f433f769c697949fdc9d58892edf
Alexander <seoful@yandex.ru>
2021-09-07 21:26:03 +0300

    fixed bubble sort

--- week03/ex2.c
+++ week03/ex2.c
@@ -2,7 +2,7 @@
 
 void bubble_sort(int *array, int size){
     for(int i = 0 ; i < size; i++){
-        for(int j = 0; j < size; j++){
+        for(int j = 0; j < size - 1; j++){
             if(array[j] > array[j + 1]){
                 int a = array[j];
                 array[j] = array[j + 1];
