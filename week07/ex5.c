e011c28906dcd6c0f8e7d2da3907f196b2dad5e6
Alexander <seoful@yandex.ru>
2021-10-05 22:57:15 +0300

    added new week

--- week07/ex5.c
+++ week07/ex5.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<cstdlib>
+#include<stdlib.h>
 int main() {
 	
     char** s = (char**)malloc(sizeof(char*));
@@ -7,7 +7,7 @@ int main() {
 	
     char foo[] = "Hello World";
 	*s = foo;
-	printf("s is %s\n", s);
+	printf("s is %s\n", *s);
 	s[0] = foo;
 	printf("s[0] is %s\n", s[0]);
 	return(0);
